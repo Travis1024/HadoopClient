4a23829e90a08c3f127810a70748a70327064f43
Travis <77580559+Travis1024@users.noreply.github.com>
2022-10-10 15:27:42 +0800

    UpdateByWindows 2022.10.10

--- src/main/java/com/hadoop/travis/Utils/EvaluationCalc.java
+++ src/main/java/com/hadoop/travis/Utils/EvaluationCalc.java
@@ -9,8 +9,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.HashMap;
-import java.util.Locale;
 import java.util.Map;
 
 /**
@@ -38,9 +38,9 @@ public class EvaluationCalc {
      * @param
      * @return void
      **/
-    public static void dealPredictResult() throws IOException {
+    public static void dealPredictResult() throws IOException, URISyntaxException, InterruptedException {
         Configuration configuration = new Configuration();
-        FileSystem fs = FileSystem.get(URI.create(ParamsClass.resultPath4 + "part-r-00000"), configuration);
+        FileSystem fs = FileSystem.get(new URI("hdfs://hadoop02:8020"), configuration, "travis01");
         FSDataInputStream fsDataInputStream = fs.open(new Path(ParamsClass.resultPath4 + "/part-r-00000"));
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(fsDataInputStream));
         String line = null;
@@ -53,7 +53,7 @@ public class EvaluationCalc {
             }
             String trueClass = results[0];
             String predictClass = results[2];
-            flag = trueClass == predictClass ? true : false;
+            flag = trueClass.equals(predictClass) ? true : false;
             if (flag){
                 for (int i = 0; i < classList.length; i++){
                     if (trueClass.equals(classList[i])){
@@ -126,12 +126,25 @@ public class EvaluationCalc {
         resultIndex.put("macroF1", top / classList.length);
     }
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, URISyntaxException, InterruptedException {
         dealPredictResult();
         calcMicroIndex();
         calcMacroIndex();
-        for (Map.Entry<String, Double> entry : resultIndex.entrySet()) {
-            System.out.println(entry.getKey() + "---" + entry.getValue());
-        }
+        System.out.println("microRecall" + "\t\t-----> " + resultIndex.get("microRecall"));
+        System.out.println("microPrecision" + "\t-----> " + resultIndex.get("microPrecision"));
+        System.out.println("microF1" + "\t\t\t-----> " + resultIndex.get("microF1"));
+        System.out.println("macroRecall0" + "\t-----> " + resultIndex.get("macroRecall0"));
+        System.out.println("macroRecall1" + "\t-----> " + resultIndex.get("macroRecall1"));
+        System.out.println("macroRecall2" + "\t-----> " + resultIndex.get("macroRecall2"));
+        System.out.println("macroRecall3" + "\t-----> " + resultIndex.get("macroRecall3"));
+        System.out.println("macroPrecision0" + "\t-----> " + resultIndex.get("macroPrecision0"));
+        System.out.println("macroPrecision1" + "\t-----> " + resultIndex.get("macroPrecision1"));
+        System.out.println("macroPrecision2" + "\t-----> " + resultIndex.get("macroPrecision2"));
+        System.out.println("macroPrecision3" + "\t-----> " + resultIndex.get("macroPrecision3"));
+        System.out.println("macroF1Score0" + "\t-----> " + resultIndex.get("macroF1Score0"));
+        System.out.println("macroF1Score1" + "\t-----> " + resultIndex.get("macroF1Score1"));
+        System.out.println("macroF1Score2" + "\t-----> " + resultIndex.get("macroF1Score2"));
+        System.out.println("macroF1Score3" + "\t-----> " + resultIndex.get("macroF1Score3"));
+        System.out.println("macroF1" + "\t\t\t-----> " + resultIndex.get("macroF1"));
     }
 }
