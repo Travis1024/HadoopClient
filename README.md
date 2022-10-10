00e7ef2fb11798d5bb74d71b1096b950de98d470
Travis <77580559+Travis1024@users.noreply.github.com>
2022-10-10 14:57:13 +0800

    UpdateByMac 2022.10.10

--- README.md
+++ README.md
@@ -1,2 +1,66 @@
 # HadoopClient
  BayesProbabilityCalc
+
+
+
+
+
+<1>--I13000
+
+<2>--I33020
+
+<3>--I75000
+
+<4>--I81402
+
+
+
+|         |       | Real | Real | Real | Real |
+| ------- | ----- | ---- | ---- | ---- | ---- |
+|         | Class | 1    | 2    | 3    | 4    |
+| Predict | 1     | a    | e    | i    | m    |
+| Predict | 2     | b    | f    | j    | n    |
+| Predict | 3     | c    | g    | k    | p    |
+| Predict | 4     | d    | h    | l    | q    |
+
+-   predictClassNums[0] = a + e + i + m
+-   predictClassNums[1] = b + f + j + n
+-   predictClassNums[2] = c + g + k + p
+-   predictClassNums[3] = d + h + l + q
+
+
+
+-   realClassNums[0] = a + b + c + d
+-   realClassNums[1] = e + f + g + h
+-   realClassNums[2] = i + j + k + l
+-   realClassNums[3] = m + n + p + q
+
+
+
+-   对于第一类：
+
+    FP1 = e + i + m
+    TP1 = a
+    FN1 = b + c + d
+    TN1 = f + g + h + j + k + l + n + p +q
+
+-   对于第二类：
+
+    FP2 = b + j + n
+    TP2 = f
+    FN2 = e + g + h
+    TN2 = a + c + d + i + k + l + m + p + q
+
+-   对于第三类：
+
+    FP3 = c + g + p
+    TP3 = k
+    FN3 = i +j +l
+    TN3 = a + b + d + e + f + h + m + n + q
+
+-   对于第四类：
+
+    FP4 = d + h + l
+    TP4 = q
+    FN4 = m + n + p
+    TN4 = a + b + c + e + f + g + i + j + k
---
+++ src/main/java/com/hadoop/travis/Utils/EvaluationCalc.java
@@ -0,0 +1,137 @@
+package com.hadoop.travis.Utils;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
+/**
+ * description: ()
+ *
+ * @author : [travis-wei]
+ * @version : [v1.0]
+ * @createTime : [2022/10/10 09:24]
+ */
+public class EvaluationCalc {
+    private static final String[] classList = {"I13000", "I33020", "I75000", "I81402"};
+    private static int[] FP = new int[4];
+    private static int[] TP = new int[4];
+    private static int[] FN = new int[4];
+    private static int[] TN = new int[4];
+    private static int totalNumber = 0;
+    private static int[] realClassNums = new int[4];
+    private static int[] predictClassNums = new int[4];
+    private static Map<String, Double> resultIndex = new HashMap<>();
+
+    /**
+     * 计算各个类别的FP、TP、FN、TN等指标
+     * @author travis-wei
+     * @createTime 2022/10/10
+     * @param
+     * @return void
+     **/
+    public static void dealPredictResult() throws IOException {
+        Configuration configuration = new Configuration();
+        FileSystem fs = FileSystem.get(URI.create(ParamsClass.resultPath4 + "part-r-00000"), configuration);
+        FSDataInputStream fsDataInputStream = fs.open(new Path(ParamsClass.resultPath4 + "/part-r-00000"));
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(fsDataInputStream));
+        String line = null;
+        boolean flag = false;
+
+        while ((line = bufferedReader.readLine()) != null){
+            String[] results = line.split("\\s");
+            for (String result : results) {
+                result.trim();
+            }
+            String trueClass = results[0];
+            String predictClass = results[2];
+            flag = trueClass == predictClass ? true : false;
+            if (flag){
+                for (int i = 0; i < classList.length; i++){
+                    if (trueClass.equals(classList[i])){
+                        realClassNums[i]++;
+                        predictClassNums[i]++;
+                        TP[i]++;
+                    }
+                }
+            }else {
+                for (int i = 0; i < classList.length; i++){
+                    if (trueClass.equals(classList[i])){
+                        realClassNums[i]++;
+                    }
+                    if (predictClass.equals(classList[i])){
+                        predictClassNums[i]++;
+                    }
+                }
+            }
+        }
+        bufferedReader.close();
+        // 所有文件数量
+        for (int i = 0; i < classList.length; i++) {
+            totalNumber += realClassNums[i];
+        }
+        // 间接计算FP、FN、TN
+        for (int i = 0; i < classList.length; i++) {
+            FP[i] = predictClassNums[i] - TP[i];
+            FN[i] = realClassNums[i] - TP[i];
+            TN[i] = totalNumber - realClassNums[i] - predictClassNums[i] + TP[i];
+        }
+    }
+
+    /**
+     * 计算微平均指标（microRecall、microPrecision、microF1）
+     * @author travis-wei
+     * @createTime 2022/10/10
+     * @param
+     * @return void
+     **/
+    public static void calcMicroIndex(){
+        int top = 0, down1 = 0, down2 = 0;
+        for (int i = 0; i < classList.length; i++) {
+            top += TP[i];
+            down1 += FN[i];
+            down2 += FP[i];
+        }
+        double microRecall = top * 1.0 / (top + down1);
+        double microPrecision = top * 1.0 / (top + down2);
+        double microF1 = 2 * microRecall * microPrecision / (microPrecision + microRecall);
+        resultIndex.put("microRecall", microRecall);
+        resultIndex.put("microPrecision", microPrecision);
+        resultIndex.put("microF1", microF1);
+    }
+
+    /**
+     * 计算宏平均指标（macroRecall、macroPrecision、macroF1Score、macroF1）
+     * @author travis-wei
+     * @createTime 2022/10/10
+     * @param
+     * @return void
+     **/
+    public static void calcMacroIndex(){
+        double top = 0.0;
+        for (int i = 0; i < classList.length; i++) {
+            resultIndex.put("macroRecall" + i, TP[i] * 1.0 / (TP[i] + FN[i]));
+            resultIndex.put("macroPrecision" + i, TP[i] * 1.0 / (TP[i] + FP[i]));
+            resultIndex.put("macroF1Score" + i, TP[i] * 2.0 / (2.0 * TP[i] + FP[i] + FN[i]));
+            top += resultIndex.get("macroF1Score" + i);
+        }
+        resultIndex.put("macroF1", top / classList.length);
+    }
+
+    public static void main(String[] args) throws IOException {
+        dealPredictResult();
+        calcMicroIndex();
+        calcMacroIndex();
+        for (Map.Entry<String, Double> entry : resultIndex.entrySet()) {
+            System.out.println(entry.getKey() + "---" + entry.getValue());
+        }
+    }
+}
